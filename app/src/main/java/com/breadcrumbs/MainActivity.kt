059bb9227be8da277813331d1aa8f882bc82491c
Arbel Charny <arbel.charny@gmail.com>
2026-04-18 17:29:17 +0300

    Feat(UI): Created bottom navbar

--- app/src/main/java/com/breadcrumbs/MainActivity.kt
+++ app/src/main/java/com/breadcrumbs/MainActivity.kt
@@ -3,11 +3,16 @@ package com.breadcrumbs
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         val auth = FirebaseAuth.getInstance()
         if (auth.currentUser == null) {
@@ -20,6 +25,18 @@ class MainActivity : AppCompatActivity() {
                 }
         }
 
-        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(navController)
+
+        bottomNav.menu.findItem(R.id.placeholder).isEnabled = false
+
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fab_add)
+        fabAdd.setOnClickListener {
+            navController.navigate(R.id.addPoiFragment)
+        }
     }
 }
